986b52f56b249c36686a1ec0cb1573d69714c687
VirginiaRool <virginia.aro@gmail.com>
2023-01-09 12:52:34 -0600

    created Profilecard component

--- src/index.js
+++ src/index.js
@@ -1,9 +1,22 @@
 //1) Import the React and reactDom libraries
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App";
 
 //2)Get a reference to the div with ID root
+// const el = document.getElementById("root");
 
 //3)Tell React to take control of that element
+// const root = ReactDOM.createRoot("el");
 
 //4) Create a component
 
-//5) Show the componen on the screen
+//5) Show the component on the screen
+// root.render(<App />);
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+);
